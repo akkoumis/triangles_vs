77eddca9a1926549ccfeb4f56093faa30cdca6c1
akkoumis <akkoumis@ece.auth.gr>
2019-10-04 20:29:59 +0300

    Fix Serial Wall Time

--- triangles_vs.cpp
+++ triangles_vs.cpp
@@ -16,6 +16,7 @@ MKL_INT m = 5, nnz = 13;
 
 int main()
 {
+	bool debugging = false;
 	int NNZ = 6629222;
 	MKL_INT sizeOfMatrix = 448695;
 	MKL_INT* row = (MKL_INT*)malloc(NNZ * sizeof(MKL_INT));
@@ -40,26 +41,27 @@ int main()
 	
 	// Creating the Sparse Matrix A in COO format
 	status = mkl_sparse_s_create_coo(&A_COO, SPARSE_INDEX_BASE_ONE, sizeOfMatrix, sizeOfMatrix, NNZ, row, col, val);
-	if (status == SPARSE_STATUS_SUCCESS)
+	if (status == SPARSE_STATUS_SUCCESS && debugging)
 		printf("Matrix A created with SUCCESS.\n");
 
 	// Convert Sparse Matrix A to CSR format
 	status = mkl_sparse_convert_csr(A_COO, SPARSE_OPERATION_NON_TRANSPOSE, &A);
-	if (status == SPARSE_STATUS_SUCCESS)
-		printf("Matrix A converted to CSR with SUCCESS.\n\n");
-
+	if (status == SPARSE_STATUS_SUCCESS && debugging)
+		printf("Matrix A converted to CSR with SUCCESS.\n");
+	status = mkl_sparse_order(A);
+	if (status == SPARSE_STATUS_SUCCESS && debugging)
+		printf("A ORDER done with SUCCESS.\n");
 
 	struct matrix_descr generalDesc;
 	generalDesc.type = SPARSE_MATRIX_TYPE_GENERAL;
 	status = mkl_sparse_spmm(SPARSE_OPERATION_NON_TRANSPOSE, A, A,&A2);
-	if (status == SPARSE_STATUS_SUCCESS)
-		printf("A^2 multiplication done with SUCCESS.\n");
+	if (status == SPARSE_STATUS_SUCCESS && debugging)
+		printf("\nA^2 multiplication done with SUCCESS.\n");
 	
+	status = mkl_sparse_order(A2);
+	if (status == SPARSE_STATUS_SUCCESS && debugging)
+		printf("A^2 ORDER done with SUCCESS.\n");
 	
-	end = clock();
-	// Calculating total time taken by the program. 
-	double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
-	printf("Multiplication time = %f\n\n", time_taken);
 
 	MKL_INT no_rows_A2, no_cols_A2, * rows_start_A2, * rows_end_A2, * col_index_A2;
 	MKL_INT no_rows_A, no_cols_A, * rows_start_A, * rows_end_A, * col_index_A;
@@ -67,18 +69,18 @@ int main()
 	sparse_index_base_t sparse_index_A2, sparse_index_A;
 
 	status = mkl_sparse_s_export_csr(A2, &sparse_index_A2, &no_rows_A2, &no_cols_A2, &rows_start_A2, &rows_end_A2, &col_index_A2, &values_A2);
-	if (status == SPARSE_STATUS_SUCCESS)
-		printf("A^2 export done with SUCCESS.\n");
+	if (status == SPARSE_STATUS_SUCCESS && debugging)
+		printf("\nA^2 export done with SUCCESS.\n");
 	//printf("%d\t%d\t%d\t%p\t%p\t%p\n", sparse_index_A2, no_rows_A2, no_cols_A2, &rows_start_A2[1], rows_end_A2, col_index_A2);
 
 	status = mkl_sparse_s_export_csr(A, &sparse_index_A, &no_rows_A, &no_cols_A, &rows_start_A, &rows_end_A, &col_index_A, &values_A);
-	if (status == SPARSE_STATUS_SUCCESS)
+	if (status == SPARSE_STATUS_SUCCESS && debugging)
 		printf("A export done with SUCCESS.\n");
 
+	/*printf("\nNNZ of A = %d\n", rows_start_A[no_rows_A] - sparse_index_A);
+	printf("NNZ of A^2 = %d\n", rows_start_A2[no_rows_A2]-sparse_index_A2);
 
-	printf("\nNNZ of A^2 = %d\n", rows_start_A2[no_rows_A2]-sparse_index_A2);
-
-	printf("Values: \t");
+	printf("\nValues: \t");
 	for (int i = 0; i <10; i++)
 	{
 		printf("%.0f\t", values_A2[i]);
@@ -104,22 +106,24 @@ int main()
 	{
 		printf("%d\t", rows_end_A2[i]);
 	}
-	printf("\n");
+	printf("\n");*/
 
-	float sum = 0;
-	for (int r_index = 0; r_index < no_rows_A; r_index++)
+
+	// Hadamard product and sum together
+	int sum = 0;
+	for (int r_index = 0; r_index < no_rows_A; r_index++)// Processing each rows of the matrices
 	{
 		int A_lower_bound = rows_start_A[r_index] - 1;
-		int A_upper_bound = rows_end_A[r_index] - 1;
+		int A_upper_bound = rows_end_A[r_index] - 2;
 		int A2_lower_bound = rows_start_A2[r_index] - 1;
-		int A2_upper_bound = rows_end_A2[r_index] - 1;
+		int A2_upper_bound = rows_end_A2[r_index] - 2;
 		
 		int A_c_index = A_lower_bound;
 		int A2_c_index = A2_lower_bound;
 
-		while (A_c_index>=A_lower_bound && A_c_index<A_upper_bound && A2_c_index >= A2_lower_bound && A2_c_index < A2_upper_bound) {
+		while (A_c_index >= A_lower_bound && A_c_index <= A_upper_bound && A2_c_index >= A2_lower_bound && A2_c_index <= A2_upper_bound) {
 			if (col_index_A[A_c_index] == col_index_A2[A2_c_index]) {
-				sum += values_A[A_c_index] * values_A2[A2_c_index];
+				sum += (int)(values_A[A_c_index] * values_A2[A2_c_index]);
 				A_c_index++;
 				A2_c_index++;
 			}
@@ -133,7 +137,12 @@ int main()
 
 	}
 
-	printf("\n\nsum = %.2f", sum);
+	end = clock();
+	// Calculating total time taken by the program. 
+	double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+	printf("Wall time = %f\n", time_taken);
+
+	printf("\n\nsum = %d", sum);
 
 	float nT = sum / 6;
 	printf("\nnT = %.2f", nT);
